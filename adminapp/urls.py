4b7bb9e9f8681e74d762eb7fc944e09046eefb5a
KASHYAPCT <kashyapchandran38@gmail.com>
2024-12-02 15:37:52 +0530

    change

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -4,5 +4,6 @@ from .import views
 
 urlpatterns = [
     path('',views.Login,name="Loginuser"),
-    path('home/',views.home,name="home")
+    path('home/',views.dashboard,name="dashboard"),
+    path('logout/',views.logout_view, name='logout'),
 ]
--- adminapp/views.py
+++ adminapp/views.py
@@ -7,7 +7,7 @@ def Login(request):
         user=authenticate(request,username=username1,password=password1)
         if user is not None and user.is_superuser==1:
             login(request,user)
-            return redirect(home)
+            return redirect(dashboard)
         elif user is not None and user.is_staff==0 and user.is_active==1:
             login(request,user)
             # return redirect(studlog)
@@ -16,6 +16,17 @@ def Login(request):
             return HttpResponse(error)
     else:
       return render(request,"login.html")
-   
-def home(request):
-    return render(request,'home.html')
+
+
+# @login_required(login_url='/auth/admin/login/')  
+def dashboard(request):
+    current_page = 'dashboard'
+    context = {
+        'current_page': current_page,
+    
+    }
+    return render(request, 'admin_app/pages/dashboard.html', context)
+
+def logout_view(request):
+    logout(request)
+    return redirect('home')
